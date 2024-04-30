01f6f8017b376316294cf4528b1dbe0a7c33d57e
Bagus Tri Prasetyo <109890610+bagustri3@users.noreply.github.com>
2024-04-30 19:09:04 +0700

    initial commit 75% done still got error

---
+++ README.md
@@ -0,0 +1,15 @@
+# Expo Router Example
+
+Use [`expo-router`](https://docs.expo.dev/router/introduction/) to build native navigation using files in the `app/` directory.
+
+## 🚀 How to use
+
+```sh
+npm run tunnel => untuk melakukan test secara online menggunakan NGROK dan tanpa local tunneling
+npm run start => untuk melakukan test secara offline menggunakan localhost tunneling
+```
+
+
+## 📝 Notes
+
+- [Expo Router: Docs](https://docs.expo.dev/router/introduction/)
---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": 'de0fa337c0msh2a47a3e06500972p1d4d04jsn31cc3f2e535b',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
